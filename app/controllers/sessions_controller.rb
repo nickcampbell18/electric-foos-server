af871c2b990b8097654cf4e01c557f75dd8caf9b
Nick Campbell <ncampbell@yammer-inc.com>
2014-07-20 18:54:04 +0100

    Syntax err

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
     @user = begin
       uid = auth_hash['uid']
       Player.find_by_yammer_uid(uid) ||
-        Player.create name: auth_hash['info']['name'],
-                      yammer_props: { yammer_uid: uid }
+        Player.create(name: auth_hash['info']['name'],
+                      yammer_props: { yammer_uid: uid })
     end
     self.current_user = @user
     redirect_to '/'
