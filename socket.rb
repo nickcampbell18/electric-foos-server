c468d8bd499b2859eb63b410fd6284c680ee416c
Nick Campbell <ncampbell@yammer-inc.com>
2014-07-22 15:30:14 +0100

    Add a sockets server

---
+++ socket.rb
@@ -0,0 +1,27 @@
+require 'em-websocket'
+require 'byebug'
+
+EM.run do
+
+  @channel = EM::Channel.new
+
+  EM::WebSocket.run(:host => "0.0.0.0", :port => 9090) do |ws|
+
+    ws.onopen {
+      sid = @channel.subscribe { |msg| ws.send msg }
+      @channel.push 'subscription %s created' % sid
+
+      ws.onmessage do |msg|
+        @channel.push "#{sid}: #{msg}"
+      end
+
+      ws.onclose {
+        @channel.unsubscribe(sid)
+      }
+    }
+
+  end
+
+  puts "Server started..."
+
+end
