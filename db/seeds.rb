9bde54140e835a10b5a1bc1d181a203b5de48165
Nick Campbell <ncampbell@yammer-inc.com>
2014-07-29 14:44:38 +0100

    Add some database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Player.create!([
+  {name: "Mario Caropreso", signatures: [], mugshot: "https://mug0.assets-yammer.com/mugshot/images/256x256/Rhd3G9PsQHZbl1mcDZqqqKQpsx50f7V9", permalink: "mcaropreso"},
+  {name: "Nick Campbell", signatures: [], mugshot: "https://mug0.assets-yammer.com/mugshot/images/256x256/DkvCd1WQQXk3Q32qGk7F-nhQc3w6Shjl", permalink: "ncampbell"},
+  {name: "Francesco Frison", signatures: [], mugshot: "https://mug0.assets-yammer.com/mugshot/images/256x256/DbGKPzNWP5ST9xhW5R4Skxr-0H680t3c", permalink: "ffrison"},
+  {name: "Ray Brooks", signatures: [], mugshot: "https://mug0.assets-yammer.com/mugshot/images/256x256/SrMqd3lr3DLJwXk1fkWTKFNFn6QzfZpl", permalink: "rbrooks"},
+  {name: "David Waller", signatures: [], mugshot: "https://mug0.assets-yammer.com/mugshot/images/256x256/ZJZ3ZCRTSdRR6lDLmVhfXdsSw3g2tz0F", permalink: "dwaller"},
+  {name: "Ben Smith", signatures: [], mugshot: "https://mug0.assets-yammer.com/mugshot/images/256x256/PVxcs-w9ltCQfDzwsFD3gnDMx-mnwFV4", permalink: "besmith"},
+  {name: "Enrico Scalavino", signatures: [], mugshot: "https://mug0.assets-yammer.com/mugshot/images/256x256/3vbD9hmNHb9T1RzHJ8DrXvscMGrn8LHC", permalink: "escalavino"},
+  {name: "Viktor Kelemen", signatures: [], mugshot: "https://mug0.assets-yammer.com/mugshot/images/256x256/3NRsNkt-wVZzx-xdG6mt6t4tDqGQM06N", permalink: "vkelemen"}
+])
