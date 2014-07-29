02d948405b5009624b50a5b7e1ebe04ddbe6aa2a
Nick Campbell <ncampbell@yammer-inc.com>
2014-07-29 18:34:04 +0100

    REMOVE WEB

--- app/controllers/web/players_controller.rb
+++
@@ -1,9 +0,0 @@
-module Web
-  class PlayersController < ApplicationController
-
-    def show
-      @player = Player.find(params[:id])
-    end
-
-  end
-end
--- app/controllers/web_controller.rb
+++
@@ -1,9 +0,0 @@
-class WebController < ApplicationController
-
-  def index
-    metrics = Metrics.new
-    @players_and_scores = metrics.top_players
-    @teams_and_scores   = metrics.top_teams
-  end
-
-end
